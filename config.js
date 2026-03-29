f5a2f935bf411bd2cb283979c4f477a6278465e8
emilyrichardssophie-web <emilyrichardssophie@gmail.com>
2026-03-28 22:16:51 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tmdbApiKey: process.env.TMDB_API_KEY,
+
+  defaultLanguage: "en",   // change this anytime
+  variant: "original",     // "original" or "alternative"
+
+  fallbackToAnyLanguage: true,
+
+  imageSize: "original" // w500, w780, original, etc.
+};
