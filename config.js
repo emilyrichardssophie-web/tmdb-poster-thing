26956887b1763d851d79fe84af7ebf7ce7d46ffe
emilyrichardssophie-web <emilyrichardssophie@gmail.com>
2026-03-29 22:02:01 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,26 @@
-module.exports = {
+require("dotenv").config();
+
+function get(key, fallback) {
+  return process.env[key] || fallback;
+}
+
+const config = {
   tmdbApiKey: process.env.TMDB_API_KEY,
 
-  defaultLanguage: process.env.DEFAULT_LANGUAGE || "en",
+  defaultLanguage: get("DEFAULT_LANGUAGE", "en"),
 
-  variant: process.env.VARIANT || "original",
+  variant: get("VARIANT", "original"), // default fallback
 
-  fallbackToAnyLanguage: process.env.FALLBACK === "true",
-
-  imageSize: process.env.IMAGE_SIZE || "original"
+  imageSize: {
+    poster: {
+      original: get("POSTER_SIZE_ORIGINAL", "w500"),
+      alternative: get("POSTER_SIZE_ALTERNATIVE", "w500")
+    },
+    backdrop: {
+      original: get("BACKDROP_SIZE_ORIGINAL", "original"),
+      alternative: get("BACKDROP_SIZE_ALTERNATIVE", "original")
+    }
+  }
 };
+
+module.exports = config;
